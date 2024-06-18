e7c222e1b8f3396d7228b6fe689a0649a629ebc2
Joseph <gamerjoewiz@gmail.com>
2024-06-18 15:47:24 -0400

    saved

--- 01_BasicsJS/09_Logical_Operators/05_Short-Circuit_Evaluation.js
+++ 01_BasicsJS/09_Logical_Operators/05_Short-Circuit_Evaluation.js
@@ -2,17 +2,17 @@
 
 // Step 1: Declare a variable 'isActiveSession' with the value 'false'.
 // Write your code below
-
+let isActiveSession = false; 
 
 // Step 2: Declare a variable 'accountLevel' with the value 'admin'.
 // Write your code below
-
+let accountLevel1 = 'admin'; 
 
 // Declare a variable 'hasResourceAccess' and use a logical AND operator '&&' to
 // determine if the user can access the resource, using short-circuit evaluation.
     // Hint: 'x' and 'y' === 'string'.
 // Write your code below
-
+const hasResourceAccess = isActiveSession && accountLevel1 === 'admin'; 
 
 
 console.log(hasResourceAccess);
