3c3e376ad4380f25b5f8a8cbbecae99a52b418ef
Joseph <gamerjoewiz@gmail.com>
2024-06-18 18:26:56 -0400

    saved

--- 03_Functions/15_Declaring_Functions/03_Arrow_Function.js
+++ 03_Functions/15_Declaring_Functions/03_Arrow_Function.js
@@ -4,7 +4,9 @@
   // Hint 1: The return should be a concatenation of the two parameters.
   // Hint 2: const functionName = (x, y) => { return ...; }
 // Write your code below
-
+const greetPerson = (first_name, last_name) => {
+  return first_name + ' ' + last_name;
+}
 
 
 console.log(greetPerson('John', 'Doe'));
