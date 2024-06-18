a111ea07d6062b9824a139cd017ee82fae9b7d83
Joseph <gamerjoewiz@gmail.com>
2024-06-18 14:24:49 -0400

    saved

--- 01_BasicsJS/04_String_Concatenation/02_Concatenate_a_greeting.js
+++ 01_BasicsJS/04_String_Concatenation/02_Concatenate_a_greeting.js
@@ -2,11 +2,11 @@
 
 // Step 1: Declare a variable 'name' with your name as a string value.
 // Write your code below
-
+const name = 'Joe'; 
 
 // Step 2: Declare a variable 'greeting' with the value of 'Hello, ' + name + '! How are you today?'
 // Write your code below
-
+const greeting = 'Hello, ' + name + '! How are you today?'; 
 
 
 console.log(greeting);
