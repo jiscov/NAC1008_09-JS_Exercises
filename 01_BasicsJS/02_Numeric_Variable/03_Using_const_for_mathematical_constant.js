96cd42c076edb73545012bf0cba57c882b355c14
Joseph <gamerjoewiz@gmail.com>
2024-06-18 13:19:55 -0400

    saved

--- 01_BasicsJS/02_Numeric_Variable/03_Using_const_for_mathematical_constant.js
+++ 01_BasicsJS/02_Numeric_Variable/03_Using_const_for_mathematical_constant.js
@@ -2,15 +2,15 @@
 
 // Step 1: Declare a const variable named 'PI' and assign it the value 3.14159.
 // Write your code below
-
+const PI = 3.14159; 
 
 // Step 2: Declare a const variable named 'radius' and assign it the value 5.
 // Write your code below
-
+const radius = 5; 
 
 // Step 3: Declare a const variable named 'area' and assign it the value of the formula: PI * (radius * radius).
 // Write your code below
-
+const area = PI * (radius * radius); 
 
 
 console.log("The area of the circle is:", area); 
