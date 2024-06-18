60c15afc7507c1fd656386404dc6ad0cbdbe3790
Joseph <gamerjoewiz@gmail.com>
2024-06-18 15:33:16 -0400

    saved

--- 01_BasicsJS/08_Comparison_Operators/07_Less_Than_or_Equal_To_Operator.js
+++ 01_BasicsJS/08_Comparison_Operators/07_Less_Than_or_Equal_To_Operator.js
@@ -2,12 +2,13 @@
 
 // Step 1: Declare two variables, 'countA' with the value 30 and 'countB' with the value 40.
 // Write your code below
-
+let countA = 30; 
+let countB = 40; 
 
 // Step 2: Declare a variable named 'isLessOrEqual' and 
     // use the less than or equal to operator '<=' to check if 'countA' is less than or equal to 'countB'.
 // Write your code below
-
+const isLessOrEqual = countA <= countB; 
 
 
 console.log(isLessOrEqual);
